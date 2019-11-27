2cfaa8875cb0e4e2a763640db561f5ddec5e5f4d
Moritz Marquardt <git@mo-mar.de>
2019-11-27 19:13:34 +0100

    Make passwords more secure
    - always include numbers in medium passwords
    - always use random word seperators (also for easy passwords)
    - captialize letters less rarely in insane mode (and capitalize them at all in insane gibberish)
    - raise the minimum character count for easy gibberish to 12

--- generate.js
+++ generate.js
@@ -50,38 +50,37 @@ async function generate(type, strength) {
             pw += r(random);
         }
     } else if (type == "gibberish") {
-        let len = [10,16,24,32][strength-1];
+        let len = [12,16,24,32][strength-1];
         let nextWord = 0;
         let startType = randrange(0, 2);
         for (let i = 0; i < len; i++) {
             let c = r(i % 2 === startType ? vocals : consonants);
             if (nextWord-- <= 0 && i < len - 2) {
-                if (pw.length) c = (
-                    strength > 1 ? r(separator) + // random separator if strength > 1, otherwise use a dash
-                    (strength > 2 && randrange(0,3) == 0 ? randrange(1,1000) + r(separator) : "") // add random numbers randomly for strong passwords
-                        : "-") + c;
-                c = c.toUpperCase();
-                nextWord = randrange(3, 7);
+                if (pw.length) c = r(separator) + // random separator
+                                   (strength > 1 && randrange(0,3) == 0 ? randrange(1,1000) + r(separator) : "") + // add random numbers randomly for strong passwords
+                                   c;
+                if (randrange(0, 2) == 0) c = c.toUpperCase();
+                nextWord = randrange(3, strength == 1 ? 5 : 7);
             }
-            pw += c;
+            pw += (strength > 3 && randrange(0, 3) == 0 ? c.toUpperCase() : c);
         }
     } else if (type.match(/^passphrase-/)) {
         let len = [2,2,3,4][strength-1];
         for (let i = 0; i < len; i++) {
             let w = r(await getWords(type.substr(11)));
             for (let j = 0; j < (strength > 3 ? w.length : strength > 1 ? 1 : 0); j++) { // capitalize first letter randomly on strength 2 or 3, capitalize everything randomly for extreme passwords
-                if (randrange(0, 2) > 0) { // capitalize
+                if (randrange(0, 3) == 0) { // capitalize
                     w = w.substr(0, j) + w.substr(j, 1).toUpperCase() + w.substr(j+1);
                 }
             }
-            pw += (i == 0 ? "" : strength < 2 ? "-" :
-                r(separator) +
-                (strength > 2 && randrange(0,3)==0 ? randrange(1,10000) + r(separator) : "")) +
-                w;
+            pw += r(separator) +
+                  (strength > 2 && randrange(0,3)==0 ? randrange(1,10000) + r(separator) : "") +
+                  w;
         }
         if (strength == 2) pw += r(separator) + randrange(1,1000);
     }
-
+    
+    if (!pw.match(/\d/) && strength > 1) pw += randrange(10, 1000);
     return pw;
 }
 
